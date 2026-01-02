42e9d995c132a79d813388bcce6bfb325350073f
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2026-01-02 12:14:02 -0600

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-3xl text-red-500 text-center font-bold'>Hola mundo</h1>
+     <p className="text-xl">fgfdgdfgdf</p>
+    </>
+  )
+}
+
+export default App
